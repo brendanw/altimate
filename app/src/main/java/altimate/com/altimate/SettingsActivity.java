4ffa024a0f852b24c68f9cdcdbcab5769c09abd3
jeanmw <jean.weatherwax@gmail.com>
2015-04-15 21:20:48 -0600

    Just added some unit options on the settings menu, haven't done anything much with them yet though.

--- app/src/main/java/altimate/com/altimate/SettingsActivity.java
+++ app/src/main/java/altimate/com/altimate/SettingsActivity.java
@@ -1,5 +1,7 @@
 package altimate.com.altimate;
 
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
@@ -13,5 +15,14 @@ public class SettingsActivity extends Activity {
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.settings);
+
+   Spinner spinner = (Spinner) findViewById(R.id.units_spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+            R.array.units_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+    spinner.setAdapter(adapter);
   }
 }
