a9d90128734b64d47963de397b67b7c6751e8aff
jeanmw <jean.weatherwax@gmail.com>
2015-04-15 21:01:08 -0600

    Altitude displayed using one of two methods for calculating altitude from pressure. Assumption of a base sea level pressure, inaccurate +/- 50 ft or more at close-to-sea level but relatively more accurate for higher altitudes. Could use airport info and/or location to make more accurate? Also some work on unit settings.

---
+++ app/src/main/java/altimate/com/altimate/SettingsActivity.java
@@ -0,0 +1,17 @@
+package altimate.com.altimate;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+
+/**
+ * Created by jeanweatherwax on 4/14/15.
+ */
+public class SettingsActivity extends Activity {
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.settings);
+  }
+}
