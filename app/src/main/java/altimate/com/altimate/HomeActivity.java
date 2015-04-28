87a8b54009328ebed42e02a4c98ec3ede3afb079
jeanmw <jean.weatherwax@gmail.com>
2015-04-27 20:04:59 -0700

    Fixed units rounding on display and cleaned up code

--- app/src/main/java/altimate/com/altimate/HomeActivity.java
+++ app/src/main/java/altimate/com/altimate/HomeActivity.java
@@ -7,9 +7,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Spinner;
-import android.widget.ArrayAdapter;
-import android.widget.RelativeLayout;
+
+import altimate.com.altimate.altimeter.AltimeterActivity;
+import altimate.com.altimate.settings.SettingsActivity;
 
 
 public class HomeActivity extends ActionBarActivity {
